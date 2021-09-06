fb3988b8cfc4b7b8473d6bffad133d134f9ddcdd
Coldplx <jjaramillod@uni.pe>
2021-09-05 19:40:47 -0500

    03 commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'jjtools',
   packages = ['jjtools'], # this must be the same as the name above
-  version = '0.2',
-  description = open('./README.md','r').read(),
-  license = open('./LICENCE.txt','r').read(),
+  version = '0.3',
+  description = 'Algunas funciones de uso comun',
+  license = 'MIT',
   author = 'Joseph Darwin Jaramillo del Aguila',
   author_email = 'jjaramillod@uni.pe',
   url = 'https://github.com/Coldplx/jjtools', # use the URL to the github repo
-  download_url = 'https://github.com/Coldplx/jjtools/tarball/0.1',
+  download_url = 'https://github.com/Coldplx/jjtools/tarball/0.3',
   keywords = ['testing', 'logging', 'example'],
   classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
                  'Programming Language :: Python :: 2.7',
